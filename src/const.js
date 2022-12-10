d4d37cb36ca2d1ba3204e0d331d1bd8366bda4a3
Anastasiia Chasovskikh <nastya.chasovskikh@gmail.com>
2022-12-10 19:48:05 +0300

    создает моковые данные

---
+++ src/const.js
@@ -0,0 +1,19 @@
+const TRAVEL_POINTS = ['Taxi', 'Bus', 'Train', 'Ship', 'Drive', 'Flight', 'Check-in', 'Sightseeing', 'Restaurant'];
+
+const CITIES = ['New York', 'Berlin', 'Paris', 'Buenos-Aires', 'San-Francisco', 'Mexico', 'Tbilisi'];
+
+const DESCRIPTIONS_MOCKING = [
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  'Cras aliquet varius magna, non porta ligula feugiat eget.',
+  'Fusce tristique felis at fermentum pharetra.',
+  'Aliquam id orci ut lectus varius viverra.',
+  'Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.',
+  'Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.',
+  'Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.',
+  'Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat.',
+  'Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.'
+];
+
+const PHOTO_LINK = 'https://loremflickr.com/248/152?random=';
+
+export {TRAVEL_POINTS, CITIES, DESCRIPTIONS_MOCKING, PHOTO_LINK};
