1a33781533a8ea0ca134a2c44d4c91fcf4c7a2c1
Anastasiia Chasovskikh <nastya.chasovskikh@gmail.com>
2023-02-10 16:32:24 +0300

    data

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import FormPresenter from './presenter/presenter.js';
 import PointsModel from './model/points-model';
 import FilterModel from './model/filter-model.js';
-import FilterPresenter from './presenter/filter-presenter.js';
 import NewPointButtonView from './view/new-point-button-view.js';
 import PointsApiService from './points-api-service.js';
 import { render } from './framework/render.js';
@@ -11,7 +10,6 @@ const AUTHORIZATION = 'Basic hS2s45tgfr4567uhg1';
 const END_POINT = 'https://19.ecmascript.pages.academy/big-trip-simple';
 
 
-const siteFiltersElement = document.querySelector('.trip-controls__filters');
 const tripEventsSection = document.querySelector('.trip-events');
 const siteHeaderElement = document.querySelector('.trip-main');
 
@@ -21,11 +19,6 @@ const pointsModel = new PointsModel({
   pointsApiService: new PointsApiService(END_POINT, AUTHORIZATION)
 });
 
-const filterPresenter = new FilterPresenter({
-  filterContainer: siteFiltersElement,
-  filterModel,
-  pointsModel
-});
 
 const formPresenter = new FormPresenter({
   formContainer: tripEventsSection,
--- src/view/edit-point-view.js
+++ src/view/edit-point-view.js
@@ -11,16 +11,35 @@ const BLANK_POINT = {
   type: 'Bus',
 };
 
-const createOfferTemplate = (offer) =>
-  `
-  <div class="event__offer-selector">
-    <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage">
-    <label class="event__offer-label" for="event-offer-luggage-1">
-      <span class="event__offer-title">${offer.title}</span>
-      &plus;&euro;&nbsp;
-      <span class="event__offer-price" pattern="^[0-9]+$">${offer.price}</span>
-    </label>
-  </div>`;
+// const createOfferTemplate = (offer) =>
+//   `
+//   <div class="event__offer-selector">
+//     <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage">
+//     <label class="event__offer-label" for="event-offer-luggage-1">
+//       <span class="event__offer-title">${offer.title}</span>
+//       &plus;&euro;&nbsp;
+//       <span class="event__offer-price" pattern="^[0-9]+$">${offer.price}</span>
+//     </label>
+//   </div>`;
+
+const createOffersTemplate = (offers, checkedOffers, isDisabled) => {
+  if (offers.length > 0) {
+    return (
+      `<section class="event__section  event__section--offers">
+        <h3 class="event__section-title  event__section-title--offers">Offers</h3>
+        <div class="event__available-offers">${offers.map(({ id, title, price }) =>`
+          <div class="event__offer-selector">
+            <input class="event__offer-checkbox  visually-hidden" id="event-offer-${id}" type="checkbox" name="event-offer-${id}" value=${id} ${checkedOffers.includes(id) ? 'checked' : ''} ${isDisabled ? 'disabled' : ''}>
+              <label class="event__offer-label" for="event-offer-${id}">
+                <span class="event__offer-title">${title}</span>
+                  &plus;&euro;&nbsp;
+                <span class="event__offer-price">${price}</span>
+              </label>
+          </div>`).join('')}
+        </div>
+      </section>`);
+  }
+};
 
 
 const createPicturesTemplate = (picture) => ` <img class="event__photo" src="${picture.src}">`;
@@ -34,16 +53,17 @@ const findOffersByID = (type, offers) => {
   }
 };
 
-const createOffers = (offers) => `<div class="event__available-offers">${offers.map((offer) => createOfferTemplate(offer)).join('')}</div>`;
 
 const createPointPictures = (pointPictures) => `<div class="event__photos-tape">${pointPictures.map((picture) => createPicturesTemplate(picture)).join('')}</div>`;
-
 
 const createFormCreationTemplate = (point, destinations, offers, isDisabled) => {
   const {dateFrom, dateTo, destinationId, type, basePrice, isSaving, isDeleting} = point;
 
 
   const offersByType = findOffersByID(type, offers).offers;
+
+  const offersTemplate = createOffersTemplate(offersByType, offers, isDisabled);
+
 
   const pointPictures = createPictures(destinationId, destinations);
 
@@ -124,12 +144,11 @@ const createFormCreationTemplate = (point, destinations, offers, isDisabled) => 
 
           <div class="event__field-group  event__field-group--time">
             <label class="visually-hidden" for="event-start-time-1">From</label>
-            <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${dayjs(dateFrom).format('HH:mm')}">
+            <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${dayjs(dateFrom).format('MM/DD/YY	HH:mm')}">
             &mdash;
             <label class="visually-hidden" for="event-end-time-1">To</label>
-            <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${dayjs(dateTo).format('HH:mm')}">
+            <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${dayjs(dateTo).format('MM/DD/YY	HH:mm')}">
           </div>
-
           <div class="event__field-group  event__field-group--price">
             <label class="event__label" for="event-price-1">
               <span class="visually-hidden">Price</span>
@@ -147,8 +166,7 @@ const createFormCreationTemplate = (point, destinations, offers, isDisabled) => 
 
         <section class="event__details">
           <section class="event__section  event__section--offers">
-            <h3 class="event__section-title  event__section-title--offers">Offers</h3>
-            ${createOffers(offersByType)}
+            ${offersTemplate}
           </section>
 
           <section class="event__section  event__section--destination">
